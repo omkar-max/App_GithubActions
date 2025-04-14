8b6d05260def74e6d2f4327c6835a3212a946091
omkarpasam11@gmail.com <omkarpasam11@gmail.com>
2025-04-14 18:35:47 -0400

    Saved src and tests folders

---
+++ tests/test_operations.py
@@ -0,0 +1,19 @@
+from src.math_operations import add, subtract
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(-1, -1) == -2
+    assert add(1.5, 2.5) == 4.0
+    assert add(1, -1) == 0  
+    assert add(1000000, 2000000) == 3000000
+    assert add(0.1, 0.2) == 0.3
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(3, 5) == -2
+    assert subtract(0, 0) == 0
+    assert subtract(-1, 0) == -1
+    assert subtract(2, 3) == -1
+    assert subtract(-1, 1) == -2
+    assert subtract(0, 0) == 0  
+    assert subtract(-1, -1) == 0
