d5b36d78a75e34d0bd645cba18c82eb980595f45
omkarpasam11@gmail.com <omkarpasam11@gmail.com>
2025-04-14 18:46:08 -0400

    Updated one failed test case

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -7,7 +7,7 @@ def test_add():
     assert add(1.5, 2.5) == 4.0
     assert add(1, -1) == 0  
     assert add(1000000, 2000000) == 3000000
-    assert add(0.1, 0.2) == 0.3
+    assert add(0.1, 0.2) == 0.30000000000000004 # Floating point precision issue
 def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(3, 5) == -2
